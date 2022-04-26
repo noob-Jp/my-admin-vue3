5df032d9cbbf4cb0d14b0d3acb9dfb101071b807
zjp <896823121@qq.com>
2022-04-26 22:22:48 +0800

    feat: 首页右侧我的项目组件

---
+++ src/components/MyProject/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="my-project" v-for="item in projectList ">
+        <el-card shadow="hover">
+            <el-tooltip :content="item.title" placement="top-start">
+                <a :href="item.url" target="_blank" style="display: block;text-align: center;">
+                    <img :src="item.img" />
+                </a>
+            </el-tooltip>
+        </el-card>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import sgblog from '../../assets/sgblog.png'
+import admin from '../../assets/admin.png'
+const projectList = reactive([
+    { img: sgblog, title: "vue2博客前台系统", url: "http://123.57.209.6:8138/articleList", username: "sg", password: "1234" },
+    { img: admin, title: "vue2后台管理系统", url: "http://123.57.209.6:8137/home", username: "zjp", password: "1234" },
+]);
+</script>
+    
+<style lang="scss" scoped>
+.my-project {
+    width: 100%;
+    img {
+        width: 90%;
+    }
+    margin-bottom: 50px;
+}
+
+
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,22 +1,32 @@
 
 <template>
-    <div class="quickNav">
+  <div class="dashboard">
+    <el-row :gutter="10">
+      <el-col :xs="18" :lg="8">
         <h3>快捷导航</h3>
-        <el-row :gutter="10">
-          <el-col :xs="18" :lg="3">
-            <QuickNav :navList="navList"></QuickNav>
-          </el-col>          
-        </el-row>
-    </div> 
-       
+        <QuickNav :navList="navList"></QuickNav>
+      </el-col>
+
+      <el-col :xs="24" :lg="6">
+      <h3>待定</h3>
+        <div class="">         
+        </div>
+      </el-col>
+      <el-col :xs="24" :lg="8">
+      <h3>我的项目</h3>
+        <div class="my-project">
+          <MyProject></MyProject>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+
 </template>
     
 <script setup>
 import QuickNav from '../../components/QuickNav/index.vue'
 import navList from '../../views/dashboard/options/navList.js'
-
+import MyProject from '../../components/MyProject/index.vue'
 </script>
-
 <style lang="scss" scoped>
-
 </style>
